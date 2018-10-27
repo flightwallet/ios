ccdd6005f080ee2f93792d3f71d117c469a08e4d
Aleksey Bykhun <theisease@gmail.com>
2018-10-27 22:37:24 +0300

    Initial Commit

---
+++ flight.wallet/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  flight.wallet
+//
+//  Created by Aleksey Bykhun on 27/10/2018.
+//  Copyright © 2018 Aleksey Bykhun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ flight.walletTests/flight_walletTests.swift
@@ -0,0 +1,34 @@
+//
+//  flight_walletTests.swift
+//  flight.walletTests
+//
+//  Created by Aleksey Bykhun on 27/10/2018.
+//  Copyright © 2018 Aleksey Bykhun. All rights reserved.
+//
+
+import XCTest
+@testable import flight_wallet
+
+class flight_walletTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
